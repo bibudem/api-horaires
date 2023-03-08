91d1404ab4f3ae1d2f3ca22d7f6215458602d722
Christian Rémillard <christian.remillard@gmail.com>
2023-03-08 16:58:47 -0500

    wip

--- app/middlewares/auth.middleware.js
+++ app/middlewares/auth.middleware.js
@@ -1,12 +1,22 @@
 import config from 'config'
 import passport from 'passport'
 import session from 'express-session'
+import memoryStore from 'memorystore'
 import { initialize, ensureAuthenticated } from '../authentication/index.js'
 import redirectRelative from '../lib/redirect-relative.js'
 
 export function useAuth(app) {
   if (config.get('security.useAuth')) {
-    app.use(session(config.get('app.sessionOptions')))
+    const MemoryStore = memoryStore(session)
+
+    app.use(
+      session({
+        ...config.get('app.sessionOptions'),
+        store: new MemoryStore({
+          checkPeriod: 86400000, // prune expired entries every 24h
+        }),
+      })
+    )
     app.use(passport.initialize())
     app.use(passport.session())
 
